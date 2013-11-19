017e1d5a2210985d3f2434ca13f95fd94ebdb758
Andrea Francia <andrea@andreafrancia.it>
2013-11-19 10:34:47 +0100

    ModelloCreato

---
+++ CalendarKata/Calendar.h
@@ -0,0 +1,13 @@
+//
+//  Calendar.h
+//  CalendarKata
+//
+//  Created by Andrea Francia on 11/19/13.
+//  Copyright (c) 2013 Andrea Francia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Calendar : NSObject
+
+@end
---
+++ CalendarKata/Calendar.m
@@ -0,0 +1,13 @@
+//
+//  Calendar.m
+//  CalendarKata
+//
+//  Created by Andrea Francia on 11/19/13.
+//  Copyright (c) 2013 Andrea Francia. All rights reserved.
+//
+
+#import "Calendar.h"
+
+@implementation Calendar
+
+@end
